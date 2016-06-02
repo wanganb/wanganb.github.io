19003dabadc0a78a1dc413311426baef55b5689d
wanganb <wanganb@126.com>
2016-06-03 00:46:18 +0800

    减少测试方块数量，并都平铺在地面上

--- project/ChatInUPC/js/main.js
+++ project/ChatInUPC/js/main.js
@@ -175,11 +175,11 @@ function init() {
         face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
         face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
     }
-    for ( var i = 0; i < 500; i ++ ) {
+    for ( var i = 0; i < 50; i ++ ) {
         material = new THREE.MeshPhongMaterial( { specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors } );
         var mesh = new THREE.Mesh( geometry, material );
         mesh.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
-        mesh.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
+        mesh.position.y = 10;//Math.floor( Math.random() * 20 ) * 20 +
         mesh.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
         scene.add( mesh );
         material.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
